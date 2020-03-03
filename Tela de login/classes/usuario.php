e45c32363ae6adc3e7607779b735de87447f7042
Marcos Vinicius <marcossilvareis.vinicios3@gmail.com>
2020-03-03 12:15:43 -0300

    /

--- Tela de login/classes/usuario.php
+++ Tela de login/classes/usuario.php
@@ -5,25 +5,21 @@ class Usuario
     private $pdo;
     public $msgErro = "";
 
-
     public function conectar($nome, $host, $usuario, $senha)
     {
-        $global $pdo;
-        try
-         {
-            $pdo = new PDO("msql:dbname=".$nome.";host=".$
-            host,$usuario, $senha);
+        global $pdo;
+        try {
+            $pdo = new PDO("mysql:dbname=" . $nome . ";host=" . $host, $usuario, $senha);
         } catch (PDOException $e) {
-            %msgErro = $e = getMessage();
+            $msgErro = $e->getMessage();
         }
     }
-    public function cadastrar($nome, $telefone, $email, $senha )
+    public function cadastrar($nome, $telefone, $email, $senha)
     {
-        $global $pdo;
-
+        global $pdo;
     }
-    public function logar()
+    public function logar($email, $senha);
     {
-        $global $pdo;
-
+        global $pdo;
+    }
 }
